bd7e26caae323e0c75e37bcd0c5facf451f04314
SpGerg <spgerghelp@gmail.com>
2024-04-22 20:01:33 +0500

    feat: added logger

--- ServerLauncher/Program.cs
+++ ServerLauncher/Program.cs
@@ -2,7 +2,14 @@
 
 public static class Program
 {
+    public static Logger Logger { get; private set; }
+
     public static void Main()
     {
+        Logger = new Logger(Directory.GetCurrentDirectory() + "/Test");
+        
+        Logger.Log("System", "Hello, world");
+
+        Console.ReadLine();
     }
 }
