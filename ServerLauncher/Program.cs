24ae3d6a93224c4cb75d4b1112125daa46ca52e0
SpGerg <spgerghelp@gmail.com>
2024-04-22 21:06:02 +0500

    refactor: убрал

--- ServerLauncher/Program.cs
+++ ServerLauncher/Program.cs
@@ -2,14 +2,8 @@
 
 public static class Program
 {
-    public static Logger Logger { get; private set; }
-
     public static void Main()
     {
-        Logger = new Logger(Directory.GetCurrentDirectory() + "/Test");
         
-        Logger.Log("System", "Hello, world");
-
-        Console.ReadLine();
     }
 }
