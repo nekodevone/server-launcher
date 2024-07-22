78218d9074b2cfba51d7e3fd5b3f48388410e10d
Izzy4ka <baielaibek@mail.ru>
2024-07-22 23:10:43 +0800

    refactor: clear unnecessary methods

--- ServerLauncher/Utility/EmptyExtensions.cs
+++ ServerLauncher/Utility/EmptyExtensions.cs
@@ -1,53 +1,15 @@
-using System.Text;
-
 namespace ServerLauncher.Utility
 {
-	public static class EmptyExtensions
-	{
-		public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
-		{
-			return !enumerable.Any();
-		}
+    public static class EmptyExtensions
+    {
+        public static bool IsEmpty<T>(this T[] array)
+        {
+            return array.Length <= 0;
+        }
 
-		public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
-		{
-			return enumerable?.IsEmpty() ?? true;
-		}
-
-		public static bool IsEmpty(this Array array)
-		{
-			return array.Length <= 0;
-		}
-
-		public static bool IsNullOrEmpty(this Array array)
-		{
-			return array?.IsEmpty() ?? true;
-		}
-
-		public static bool IsEmpty<T>(this T[] array)
-		{
-			return array.Length <= 0;
-		}
-
-		public static bool IsNullOrEmpty<T>(this T[] array)
-		{
-			return array?.IsEmpty() ?? true;
-		}
-
-		public static bool IsEmpty<T>(this ICollection<T> collection)
-		{
-			return collection.Count <= 0;
-		}
-
-		public static bool IsNullOrEmpty<T>(this ICollection<T> collection)
-		{
-			return collection?.IsEmpty() ?? true;
-		}
-		
-
-		public static bool IsEmpty(this StringBuilder stringBuilder)
-		{
-			return stringBuilder.Length <= 0;
-		}
-	}
+        public static bool IsNullOrEmpty<T>(this T[] array)
+        {
+            return array?.IsEmpty() ?? true;
+        }
+    }
 }
